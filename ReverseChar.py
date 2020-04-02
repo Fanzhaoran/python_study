424a1c3d8c9394b59273008b31cebe6b1217d279
Fanzhaoran <38691766+Fanzhaoran@users.noreply.github.com>
2020-04-02 15:41:07 +0800

    Update ReverseChar.py

--- ReverseChar.py
+++ ReverseChar.py
@@ -4,7 +4,7 @@ def func():
     # please finish the function body here.
     # please define the python3 output here. For example: print().
     print("".join([i.lower() if ord(i)<=90 else i.upper() for i in input().strip()]))
-    # input().strip().swap()
+    # input().strip().swapcase()
     
 if __name__ == "__main__":
     func()
