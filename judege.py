da0296902573eecda3314ef5942d6bfdc3e91598
fanzhaoran <fanzhaoran@cmict.chinamobile.com>
2021-10-11 16:08:23 +0800

    修改judge

--- judege.py
+++ judege.py
@@ -1,10 +1,11 @@
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        
-        if not (l1 and l2):
-            return l1 or l2
-            
+def fun(l1, l2):
+    if not l1:
+        return l2
+    if not l2:
+        return l1
+
+    if not (l1 and l2):
+        return l1 or l2
+
             
         # 两个判断式等价
